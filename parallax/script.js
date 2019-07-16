0a9d88152a9ebb50678bd9f5e3ebabdf8e549045
Calin.Florescu@travelport.com <calin.florescu@arobs.com>
2019-07-16 14:00:40 +0300

    static background moving elements

---
+++ parallax/script.js
@@ -0,0 +1,7 @@
+window.addEventListener('scroll', () => {
+   let wScroll = this.pageYOffset;
+
+   document.querySelector('.logo').style.transform = `translate(0px, ${wScroll/2}%)`;
+   document.querySelector('.back-bird').style.transform = `translate(0px, ${wScroll/4}%)`;
+   document.querySelector('.fore-bird').style.transform = `translate(0px, -${wScroll/35}%)`;
+});
